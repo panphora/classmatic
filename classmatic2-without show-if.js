d0f647619665780b3c50a9f6320f560d1a936e1d
David Miranda <david@storylog.com>
2023-08-25 13:54:30 -0400

    Create classmatic2-without show-if.js

---
+++ classmatic2-without show-if.js
@@ -0,0 +1,172 @@
+  (function () {
+    let clickSelectors = [
+      "[data-click-to-add-class]",
+      "[data-click-to-remove-class]",
+      "[data-click-to-toggle-class]"
+    ];
+
+    let clickAwaySelectors = [
+      "[data-click-away-to-add-class]",
+      "[data-click-away-to-remove-class]",
+      "[data-click-away-to-toggle-class]"
+    ];
+
+    let actionFunctionLookup = {
+      "[data-click-to-add-class]": ({selector, className}) => {
+        manipulateClass({selector, className, actionType: "add"});
+      },
+      "[data-click-away-to-add-class]": ({selector, className}) => {
+        manipulateClass({selector, className, actionType: "add"});
+      },
+      "[data-click-to-remove-class]": ({selector, className}) => {
+        manipulateClass({selector, className, actionType: "remove"});
+      },
+      "[data-click-away-to-remove-class]": ({selector, className}) => {
+        manipulateClass({selector, className, actionType: "remove"});
+      },
+      "[data-click-to-toggle-class]": ({selector, className}) => {
+        manipulateClass({selector, className, actionType: "toggle"});
+      },
+      "[data-click-away-to-toggle-class]": ({selector, className}) => {
+        manipulateClass({selector, className, actionType: "toggle"});
+      }
+    };
+
+    function manipulateClass ({selector, className, actionType}) {
+      let elems = Array.from(document.querySelectorAll(selector));
+
+      elems.forEach(elem => {
+        if (actionType === "remove" || actionType === "toggle" && elem.classList.contains(className)) {
+          elem.classList.remove(className);
+        } else if (actionType === "add" || actionType === "toggle" && !elem.classList.contains(className)) {
+          elem.classList.add(className);
+        }
+      });
+    }
+
+    document.addEventListener("click", (event) => {
+      let clickActions = getAllClickActions(event.target, clickSelectors.join(","));
+      // console.log("clickActions", clickActions);
+      clickActions.forEach(clickAction => clickAction.actionFunc(clickAction.actionArgs));
+
+      let clickAwayActions = getAllClickAwayActions(event.target, clickActions);
+      // console.log("clickAwayActions", clickAwayActions);
+      clickAwayActions.forEach(clickAwayAction => clickAwayAction.actionFunc(clickAwayAction.actionArgs));
+    });
+
+    function getAllClickActions (elem, selector) {
+      let parent = elem.parentNode;
+      let actions = [];
+
+      if (!selector || elem.matches(selector)) {
+        let actionDetails = getActionDetailsFromElem(elem, clickSelectors);
+        actions = actions.concat(actionDetails);
+      }
+
+      while (parent !== document) {
+        if (!selector || parent.matches(selector)) {
+          let actionDetails = getActionDetailsFromElem(parent, clickSelectors);
+          actions = actions.concat(actionDetails);
+        }
+        parent = parent.parentNode;
+      }
+
+      return actions;
+    }
+
+    function getActionDetailsFromElem (elem, selectorsList) {
+      let matchingSelectors = getMatchingSelectors(elem, selectorsList);
+      return matchingSelectors.reduce((arr, selector) => {
+        let attrName = selector.replace(/^\[+|\]+$/g, "");
+        let attrValue = elem.getAttribute(attrName);
+        let actionArgs = getArgsFromString(attrValue);
+        let actionFunc = actionFunctionLookup[selector];
+        if (Array.isArray(actionArgs)) {
+          actionArgs.forEach(args => {
+            arr.push({elem, selector, attrName, attrValue, actionArgs: args, actionFunc});
+          });
+        } else {
+          arr.push({elem, selector, attrName, attrValue, actionArgs, actionFunc});
+        }
+        return arr;
+      }, []);
+    }
+
+    function getAllClickAwayActions (clickedElem, clickActions) {
+      let clickedElems = getSelfAndAllAncestors(clickedElem);
+      let clickAwayElems = Array.from(document.querySelectorAll(clickAwaySelectors.join(",")));
+      let clickAwayElemsNotClickedOn = clickAwayElems.filter(el => !clickedElems.includes(el));
+
+      return clickAwayElemsNotClickedOn.reduce((arr, elem) => {
+        let actionDetails = getActionDetailsFromElem(elem, clickAwaySelectors);
+        // filter out matching actions (same selector and class name) 
+        // that have already been triggered by click actions
+        let actionDetailsFiltered = actionDetails.filter(awayAction => {
+          let matchingClickAction = clickActions.find(clickAction => {
+            let selectorMatch = clickAction.actionArgs.selector === awayAction.actionArgs.selector;
+            let classMatch = clickAction.actionArgs.className === awayAction.actionArgs.className;
+            return selectorMatch && classMatch;
+          });
+          return !matchingClickAction;
+        });
+        return arr.concat(actionDetailsFiltered);
+      }, []);
+    }
+
+    function getSelfAndAllAncestors (elem) {
+      let parents = [elem];
+      let parent = elem.parentNode;
+      while (parent !== document) {
+        parents.push(parent);
+        parent = parent.parentNode;
+      }
+      return parents;
+    }
+
+    function getMatchingSelectors (elem, selectors) {
+      return selectors.filter(selector => elem.matches(selector));
+    }
+
+    function getArgsFromString (str) {
+      let args = str.split(" ");
+
+      if (args.length === 1) {
+        args.push("body");
+      }
+
+      let [className, selector] = args;
+      
+      if (className.startsWith("[") || selector.startsWith("[")) {
+        let classNames = className.replace(/\s/, "").replace(/^\[+|\]+$/g, "").split(",");
+        let selectors = selector.replace(/\s/, "").replace(/^\[+|\]+$/g, "").split(",");
+        return classNames.map((c, index) => ({className: c, selector: selectors[index] || "body"}));
+      }
+
+      return {selector, className};
+    }
+
+    function onDomReady (callback) {
+      if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", callback)
+      } else {
+        callback();
+      }
+    }
+
+    document.head.insertAdjacentHTML("beforeend", "<style>[data-hide-if] {display: none;}</style>");
+    onDomReady(() => {
+      let visibilityStyles = Array.from(document.querySelectorAll("[data-hide-if]")).map(showIfElem => {
+        let hideSelector = showIfElem.getAttribute("data-hide-if");
+        let hideStyles = "";
+
+        if (hideSelector) {
+          hideStyles = `:not(${hideSelector}) [data-hide-if="${hideSelector}"], [data-hide-if="${hideSelector}"]:not(${hideSelector}) {display: block;}`;
+        }
+
+        return hideStyles;
+      }).join("");
+
+      document.head.insertAdjacentHTML("beforeend", "<style>" + visibilityStyles + "</style>");
+    });
+    
+  })()
